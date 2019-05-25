94fe81dd5784edb5060ad1f9a07727773348982f
Maitreya <47271392+MaitreyaSahu@users.noreply.github.com>
2019-05-26 01:36:47 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,69 +1,179 @@
-body {
-    font-size: 50px;
+var grid = [];
+var transGrid = [];
+var num = 1;
+var count = 1;
+var retArr = [];
+
+function getFirstRow() {
+    var i = 0;
+    var j = 0;
+    do {
+        var x = (Math.round(Math.random() * 8) + 1);
+        if (grid[0].indexOf(x) == -1) {
+            grid[i][j] = x;
+            transGrid[j++][i] = x;
+        }
+    } while (j < 9)
 }
 
-.locked-cell {
-    font-weight: bold;
+function createGrid() {
+    for (var i = 0; i < 9; i++) {
+        var arr = [];
+        for (var j = 0; j < 9; j++) {
+            arr.push(0);
+        }
+        grid.push(arr.slice());
+        transGrid.push(arr.slice());
+    }
 }
 
-.unLocked-cell {
-    color: #49a89d;
+function startGame() {
+
+    loadGrid();
+
+    $('#mainGrid td').each(function() {
+        if (Math.random() > 0.5) {
+            $(this).html('').removeClass('locked-cell').addClass('unLocked-cell');
+        }
+    });
+
 }
 
-.error-cell {
-    color: #8c061d;
+function autoSolve() {
+    $('#mainGrid td.unLocked-cell').each(function() {
+        var id = $(this).attr('id');
+        var i = id.substring(1, 2);
+        var j = id.substring(2, 3);
+        $(this).html(grid[i][j]).removeClass('error-cell');
+    });
 }
 
-/*
-.selected {
-    background-color: #5e89a8;
-}
-*/
-.selected-row, .selected-column {
-    background-color: #ebefc9;
+function loadGrid() {
+    grid = [];
+    transGrid = [];
+    retArr = [1, 0];
+
+    createGrid();
+
+    getFirstRow();
+
+    do {
+
+        retArr = solve(retArr[0], retArr[1]);
+
+    } while (retArr[0] != 'x');console.table(grid);
+
+    createTable(grid);
 }
 
-table {
-    border: 10px solid;
-    margin: 1em auto;
-    /* width: 100%; */
-    border-collapse: collapse;
-    /* height: 100%; */
+function createTable(tableData) {
+    $("#mainGrid tbody tr").each(function(rowIndex) {
+        var rowData = grid[rowIndex];
+        $(this).find("td").each(function(cellIndex) {
+            $(this).text(rowData[cellIndex]).addClass('locked-cell').removeClass('unLocked-cell');
+        });
+    })
 }
 
-td {
-    height: 80px;
-    width: 80px;
-    border: 1px solid #ccc;
-    text-align: center;
+function isValidInRow(gridVal, gridRow) {
+    return grid[gridRow].indexOf(gridVal) == -1 ? true : false;
 }
 
-#mainGrid td:nth-child(3n) {
-    border-right: 5px solid black;
+function isValidInColumn(gridVal, gridColumn) {
+    return transGrid[gridColumn].indexOf(gridVal) == -1 ? true : false;
 }
 
-tr:nth-child(3n) td {
-    border-bottom: 5px solid black;
+function isValidInBox(gridVal, gridRow, gridColumn) {
+    var boxIndexI = Math.round((gridRow - 1) / 3) * 3;
+    var boxIndexJ = Math.round((gridColumn - 1) / 3) * 3;
+    for (var i = boxIndexI; i < boxIndexI + 3; i++) {
+        for (var j = boxIndexJ; j < boxIndexJ + 3; j++) {
+            if (grid[i][j] == gridVal) {
+                return false;
+            }
+        }
+    }
+    return true;
 }
 
-#nums-table td {
-    border: 10px solid black;
+function getPossibleValues(posI, posJ) {
+    var possibleValues = [];
+    for (var i = 1; i < 10; i++) {
+        if (isValidInRow(i, posI) && isValidInColumn(i, posJ) && isValidInBox(i, posI, posJ)) {
+            possibleValues.push(i);
+        }
+    }
+    return possibleValues;
 }
 
-.btn {
-    background-color: #a5c47d;
-    /* Green */
-    border: none;
-    color: white;
-    padding: 10px 65px;
-    text-align: center;
-    text-decoration: none;
-    display: inline-block;
-    font-size: 75px;
-    margin: 5px 50px;
-    cursor: pointer;
+function solve(posI, posJ) {
+    var index;
+    num = grid[posI][posJ];
+    grid[posI][posJ] = 0;
+    transGrid[posJ][posI] = 0;
+
+    var possibleValues = getPossibleValues(posI, posJ);
+    index = possibleValues.indexOf(num);
+    if (possibleValues.length == 0 || (index == (possibleValues.length - 1))) {
+        return [posJ == 0 ? --posI : posI, posJ == 0 ? 8 : --posJ];
+    } else {
+        num = possibleValues[index + 1];
+        grid[posI][posJ] = num;
+        transGrid[posJ][posI] = num;
+        if (posI == 8 && posJ == 8) {
+            return ['x', 'x'];
+        }
+        return [posJ == 8 ? ++posI : posI, posJ == 8 ? 0 : ++posJ];
+
+    }
+
 }
 
-.container {
-    text-align: center;
+$(document).ready(function() {
+
+    $("#mainGrid td").hover(function() {
+        selectionOn(this);
+    }, function() {
+        selectionRemoved(this);
+    });
+
+    $("#mainGrid td").click(function() {
+        //selectionOn(this);
+        $('#mainGrid td.selected').removeClass('selected');
+        var index = $(this).index();
+        index++;
+        $(this).addClass('selected');
+        $(this).closest('tr').addClass('selected-row');
+        $("#mainGrid tr td:nth-child(" + index + ")").addClass('selected-row');
+    });
+
+    $("#nums-table td").click(function() {
+        var val = $(this).html();
+        var selected = $('#mainGrid td.selected').html(val);
+        var id = '';
+        id = $(selected).attr('id');
+        var i = id.substring(1, 2);
+        var j = id.substring(2, 3);
+        grid[i][j] != val ? $(selected).addClass('error-cell') : $(selected).removeClass('error-cell');
+
+    });
+
+});
+
+function selectionOn(selectedCell) {
+    var index = $(selectedCell).index();
+    index++;
+    //$(selectedCell).addClass('selected');
+    $(selectedCell).closest('tr').addClass('selected-row');
+    $("#mainGrid tr td:nth-child(" + index + ")").addClass('selected-row');
+
+}
+
+function selectionRemoved(selectedCell) {
+    var index = $(selectedCell).index();
+    index++;
+    //$(selectedCell).removeClass('selected');
+    $(selectedCell).closest('tr').removeClass('selected-row');
+    $("#mainGrid tr td:nth-child(" + index + ")").removeClass('selected-row');
+
 }
